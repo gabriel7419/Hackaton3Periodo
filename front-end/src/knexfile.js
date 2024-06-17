86f6911ad3bbc8922530f07dbf81ed9b57f71997
gabriel7419 <sgtgabriel7419@gmail.com>
2024-06-17 20:49:39 -0300

    Atualizado os arquivos após o merge para evitar conflitos

---
+++ front-end/src/knexfile.js
@@ -0,0 +1,13 @@
+module.exports = {
+  development: {
+    client: 'mysql',
+    connection: {
+      host: 'localhost',
+      port: 3306,
+      user: 'root',
+      password: 'password',
+      database: 'your_database_name',
+    },
+  },
+};
+
