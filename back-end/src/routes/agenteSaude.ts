5dc83559c4ce2da0cd841673f1f02a465665a7b3
RaInaldo <rodrigoinaldo281@gmail.com>
2024-06-18 11:10:52 -0300

    git

--- back-end/src/routes/agenteSaude.ts
+++ back-end/src/routes/agenteSaude.ts
@@ -1,9 +1,9 @@
-import Router, { Request, Response } from "express"
-import db from '../database/db'
+// import Router, { Request, Response } from "express"
+// import db from '../database/db'
 
-const routes = Router()
+// const routes = Router()
 
-routes.get("/", (req,res) =>{
-    const sql= 'SELECT * FROM agentesaude'
-    db.connection.query()
-})
+// routes.get("/", (req,res) =>{
+//     const sql= 'SELECT * FROM agentesaude'
+//     db.query()
+// })
--- back-end/src/routes/usuario.ts
+++ back-end/src/routes/usuario.ts
@@ -1,91 +1,91 @@
-import Router from 'express'
-import knex from '../database/knex'
-import AppError from '../utils/AppError';
-import { hash } from 'bcrypt'
-import { z } from "zod"
+// import Router from 'express'
+// import knex from '../database/knex'
+// import AppError from '../utils/AppError';
+// import { hash } from 'bcrypt'
+// import { z } from "zod"
 
-const router = Router();
+// const router = Router();
 
-router.post("/", async (req, res) => {
+// router.post("/", async (req, res) => {
 
-    const registerBodySchema = z.object({
-        nome: z.string(),
-        email: z.string().email(),
-        senha: z.string(
-            {message:"Senha é obrigatoria"}
-        ).min(6)
-    })
+//     const registerBodySchema = z.object({
+//         nome: z.string(),
+//         email: z.string().email(),
+//         senha: z.string(
+//             {message:"Senha é obrigatoria"}
+//         ).min(6)
+//     })
 
-    const objSalvar = registerBodySchema.parse(
-        req.body
-    );
+//     const objSalvar = registerBodySchema.parse(
+//         req.body
+//     );
 
-    if (!objSalvar?.senha) {
-        throw new AppError("Senha Obrigatoria")
-    }
+//     if (!objSalvar?.senha) {
+//         throw new AppError("Senha Obrigatoria")
+//     }
 
-    objSalvar.senha = await hash(objSalvar.senha, 8)
+//     objSalvar.senha = await hash(objSalvar.senha, 8)
 
-    // Promise - async
-    const id_usuario = await knex('usuarios').insert(objSalvar)
+//     // Promise - async
+//     const id_usuario = await knex('usuarios').insert(objSalvar)
 
-    const usuarios = await knex('usuarios')
-        .where({
-            id: id_usuario[0]
-        }); res.json({ usuarios: usuarios })
-})
+//     const usuarios = await knex('usuarios')
+//         .where({
+//             id: id_usuario[0]
+//         }); res.json({ usuarios: usuarios })
+// })
 
-router.get("/", (req, res) => {
-    knex('usuarios').then((resposta) => {
-
-
-        res.json({ usuarios: resposta })
-    })
+// router.get("/", (req, res) => {
+//     knex('usuarios').then((resposta) => {
 
 
-})
-
-router.put('/:id', async (req, res) => {
-    const objSalvar = req.body;
-
-    const { id } = req.params
-
-    if (objSalvar?.senha) {
-        objSalvar.senha = await hash(objSalvar.senha, 8)
-    }
-
-    let usuario = await knex('usuarios').where({ id }).first()
-
-    if (!usuario?.id) {
-        throw new AppError('Usuário não encontrado!')
-    }
-
-    let newUsuario = {
-        ...usuario,
-        ...objSalvar
-    }
-
-    await knex('usuarios').update(newUsuario)
-        .where({ id: usuario.id })
-
-    return res.json({
-        message: `Usuario alterado com Sucesso`
-    })
-})
-
-router.delete('/:id', async (req, res) => {
-    const { id } = req.params
-
-    const usuario = await knex('usuarios').where({ id }).first()
-
-    if (!usuario) {
-        throw new AppError('Usuario não encontrado', 404)
-    }
-
-    await knex('usuarios').where({ id }).delete()
-
-    res.json({ message: "usuario deletado com sucesso" })
-})
+//         res.json({ usuarios: resposta })
+//     })
 
 
-export default router
+// })
+
+// router.put('/:id', async (req, res) => {
+//     const objSalvar = req.body;
+
+//     const { id } = req.params
+
+//     if (objSalvar?.senha) {
+//         objSalvar.senha = await hash(objSalvar.senha, 8)
+//     }
+
+//     let usuario = await knex('usuarios').where({ id }).first()
+
+//     if (!usuario?.id) {
+//         throw new AppError('Usuário não encontrado!')
+//     }
+
+//     let newUsuario = {
+//         ...usuario,
+//         ...objSalvar
+//     }
+
+//     await knex('usuarios').update(newUsuario)
+//         .where({ id: usuario.id })
+
+//     return res.json({
+//         message: `Usuario alterado com Sucesso`
+//     })
+// })
+
+// router.delete('/:id', async (req, res) => {
+//     const { id } = req.params
+
+//     const usuario = await knex('usuarios').where({ id }).first()
+
+//     if (!usuario) {
+//         throw new AppError('Usuario não encontrado', 404)
+//     }
+
+//     await knex('usuarios').where({ id }).delete()
+
+//     res.json({ message: "usuario deletado com sucesso" })
+// })
+
+
+// export default router
