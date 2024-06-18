3e846bc9b393d8163a12fbd47778f527e140bba8
gabriel7419 <sgtgabriel7419@gmail.com>
2024-06-18 20:13:10 -0300

    Criado as pastas knex e migrations para conexão com o banco de dados do Node.

--- back-end/src/routes/index.ts
+++ back-end/src/routes/index.ts
@@ -1,7 +1,7 @@
 import { Router } from "express"
 
-import categoria from './categoria'
-import usuario from './usuario'
+// import categoria from './categoria'
+// import usuario from './usuario'
 import session from './session'
 import autenticacao from '../middlewares/autenticacao'
 
@@ -9,7 +9,7 @@ const routes = Router()
 
 routes.use('/session', session)
 routes.use(autenticacao)
-routes.use('/categorias', autenticacao, categoria)
-routes.use('/usuarios', usuario)
+// routes.use('/categorias', autenticacao, categoria)
+// routes.use('/usuarios', usuario)
 
 export default routes
