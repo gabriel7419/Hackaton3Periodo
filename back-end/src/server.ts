d2448bdff52a0a1bd00948b8b9b7fc304246c550
Rodrigo <rodrigoinaldo281@email.com>
2024-06-18 20:15:02 -0300

    api

---
+++ back-end/src/server.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response, NextFunction } from 'express'
+import 'express-async-errors'
+import cors from "cors"
+import routes from './routes'
+
+const app = express()
+
+app.use(cors())//consumo de api habilitado para outras plataformas
+app.use(express.json())
+app.use(routes)
+
+
+const PORT = 8000
+app.listen(PORT, () => {
+    console.log('Express Iniciado na porta:' + PORT)
+})
