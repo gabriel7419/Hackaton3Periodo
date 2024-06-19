bb1db3d990c4311903e1af6649c31e42f0ece94e
gabriel7419 <sgtgabriel7419@gmail.com>
2024-06-19 19:28:29 -0300

    Corrigida identação do arquivo index.ts dentro da pasta de database

--- back-end/src/database/index.ts
+++ back-end/src/database/index.ts
@@ -1,10 +1,10 @@
 import * as mysql from 'mysql2/promise';
 
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'vacinacao'
+host: 'localhost',
+user: 'root',
+password: '',
+database: 'vacinacao'
 });
 
 export default db;
