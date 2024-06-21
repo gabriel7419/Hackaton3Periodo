8ee094807874f788583ca89895819527df437754
gabriel7419 <sgtgabriel7419@gmail.com>
2024-06-21 20:42:21 -0300

    Modificado o README.md e o footer.php do front-end, retirando o texto de exemplo e informações errôneas do README.md

--- front-end/admin/footer.php
+++ front-end/admin/footer.php
@@ -1,44 +1,43 @@
-
 <footer>
-  <div>
-    <h1>Exemplo de Footer</h1>
-  </div>
+    <div>
+        <h1>ConnectVax - Levando Saúde para Você</h1>
+    </div>
 
-  <div>
-    <p>Este é o conteúdo principal da página.</p>
-  </div>
+    <div>
+        <p>Sistema Integrado de Saúde</p>
+    </div>
 
     <div class="container">
-      <div class="row">
-        <div class="col-sm-4">
-          <img src="logo.png" alt="Logotipo">
+        <div class="row">
+            <div class="col-sm-4">
+                <img src="logo.png" alt="Logotipo">
+            </div>
+            <div class="col-sm-4">
+                <nav class="footer-nav">
+                    <ul>
+                        <li><a href="./paginas/paginalogado.php">Home</a></li>
+                        <li><a href="./paginas/login.php">Login</a></li>
+                        <li><a href="./paginas/registro.php">Cadastrar-se</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="col-sm-4">
+                <p>
+                    <strong>ConnectVax</strong><br>
+                    Cidade: Umuarama, PR<br>
+                    Telefone: (44) 1234-5678<br>
+                    Email: contato@connectvax.com.br<br>
+                    Atendimento do Suporte: Segunda a sexta, das 8h às 18h
+                </p>
+            </div>
         </div>
-        <div class="col-sm-4">
-          <nav class="footer-nav">
-            <ul>
-              <li><a href="#">Página 1</a></li>
-              <li><a href="#">Página 2</a></li>
-              <li><a href="#">Página 3</a></li>
-            </ul>
-          </nav>
-        </div>
-        <div class="col-sm-4">
-          <p>
-            <strong>Empresa XYZ</strong><br>
-            Endereço: Rua Exemplo, 123<br>
-            Cidade: Exemplo, SP<br>
-            Telefone: (11) 1234-5678<br>
-            Email: contato@exemplo.com.br<br>
-            Atendimento: Segunda a sexta, das 8h às 18h
-          </p>
-        </div>
-      </div>
     </div>
     <div class="copyright">
-      <p>&copy; 2024 Empresa XYZ - Todos os direitos reservados</p>
-      <p>Desenvolvido por Desenvolvedor Exemplo</p>
+        <p>&copy; 2024 ConnectVax - Todos os direitos reservados</p>
+        <p>Desenvolvido por Muita Gente</p>
     </div>
-  </footer>
+</footer>
 
 </body>
+
 </html>
